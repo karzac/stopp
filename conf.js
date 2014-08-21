21970da69c4a94701ac918c9470b50f2ddf3c82d
dav- <derrick@cohodas.me>
2014-08-21 07:26:35 -0700

    Refactor parseHostsSync; Improve help; Add colors!; Clean up

--- conf.js
+++ conf.js
@@ -1,28 +1,38 @@
-var pkg = require("./package.json");
+var pkg    = require("./package.json")
+  , colors = require("colors")
+
 module.exports = {
-    help: [ // Each array element will be printed on its own line
-            "Stopp: Stop Procrastination. (v" + pkg.version + ")"
-          , "Copyright (c) 2014 Derrick Cohodas, All Rights Reserved. See https://github.com/dav-/stopp/LICENSE.md for license information."
+    startBanner: "#### START STOPP CONFIGURATION - DO NOT MODIFY THIS LINE OR BELOW ####"
+  , endBanner: "#### END STOPP CONFIGURATION - DO NOT MODIFY THIS LINE OR ABOVE ####"
+  , backups: true
+  , backupDir: process.env.PWD + '/backups/'
+  , rerouteIP: '0.0.0.0'
+  , hostsLocation: process.env.PWD + '/hosts' //'/private/etc/hosts'
+
+  , help: [ // Each array element will be printed on its own line
+            ""
+          , ("Stopp: Stop Procrastination. (v" + pkg.version + ")").cyan
+          , "Copyright (c) 2014 Derrick Cohodas, All Rights Reserved. See "+"https://github.com/dav-/stopp/LICENSE.md".underline+" for license information."
           , ""
-          , "WARNING: Use at your own risk. This program modifies the HOSTS file on your system."
+          , "WARNING: Use at your own risk. This program modifies the HOSTS file on your system.".red
           , "Author(s) assume no responsibility or liability for any damages that may occur as a result, direct or indirect, of the use of this software."
           , ""  
-          , "NOTE: Do not modify your HOSTS file while the filter is ON!"  
+          , "WARNING: It is not recommended to modify your HOSTS file while the filter is ON!".red  
           , ""
-          , "Usage: stopp <command> [args]"
-          , "Example: stopp add reddit.com"
+          , "Usage: stopp <command> [args]".yellow
+          , "Example: "+"stopp add reddit.com".cyan
+          , "Example: "+"stopp del youtube.com www.youtube.com facebook.com".cyan
           , ""
-          , "Commands:"
-          , "help                Show this menu"
-          , "on                  Turn the filter on"
-          , "off                 Turn the filter off"
-          , "status              Show the filter status"
-          , "list                List all filtered addresses"
-          , "add [addresses]     Add specified address(es) to filter. IP or URL, do not include http(s)" 
-          , "del [addresses]     Remove specified address(es) from filter"
+          , "Commands:".yellow
+          , "\thelp".cyan+"                            Show this menu"
+          , "\ton".cyan+"                              Turn the filter on"
+          , "\toff".cyan+"                             Turn the filter off"
+          , "\tstatus".cyan+"                          Show the filter status"
+          , "\tlist".cyan+"                            List all filtered addresses"
+          , "\tadd <domain> [<domain> ...]   ".cyan+"  Add specified domain(s) to filter (don't include http(s)://)." 
+          , "\tdel <domain> [<domain> ...]".cyan+"     Remove specified domain(s) from filter"
+          , ""
+          , "NOTE: Subdomains are treated as unique, so adding an entry for \"reddit.com\" will not block \"www.reddit.com\""
+          , ""
           ]
-  , startBanner: "#### START STOPP CONFIGURATION - DO NOT MODIFY THIS LINE OR BELOW ####"
-  , endBanner: "#### END STOPP CONFIGURATION - DO NOT MODIFY THIS LINE OR ABOVE ####"
- 
-
 }
--- index.js
+++ index.js
@@ -25,12 +25,19 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
+ *
+ * --
+ *
+ * Notes:
+ * 
+ *  + This code is all synchronous, as there is no real performance benefiet
+ *    to async in this use case
+ *    
+ *  + This application is currently OSX-only. Feel free to submit a pull request
+ *    with support for other OSes, but until then it will die on any other OS
  * 
  */
 
-// This application is currently OSX-only. 
-// Feel free to submit a pull request with support for other OSes,
-// but until then it will die on any other OS
 
 if(process.env["_system_name"] !== 'OSX'){
     console.log("Sorry, your OS is unsupported. Feel free to submit a pull request :)")
@@ -38,10 +45,15 @@ if(process.env["_system_name"] !== 'OSX'){
 }
 
 
-var yargs   = require("yargs")
-  , conf    = require("./conf.js")
+var conf    = require("./conf.js")
+  , pkg     = require("./package.json")
+  , yargs   = require("yargs")
   , fs      = require("fs")
   , fs_sync = require("fs-sync")
+  , colors  = require("colors")
+
+// Don't allow this package to be required 
+module.exports = new Error("The package 'stopp' cannot be required. This is a global-only command line utility. See " + pkg.homepage + " for usage information.")
 
 /**
  * Stopp Class
@@ -55,76 +67,137 @@ function Stopp(argv){
     // ************** Private methods ************** //
 
     /**
-     * Backs up the hosts file to ./bak/hosts-[unix_timestamp].bak. This function is synchronous.
+     * Backs up the hosts file to ./bak/hosts-Day-Mon-DD-YYYY-HH.MM.SS.bak. This function is synchronous.
      * @return {string} Path and name of the backup
      */
-    var backupHostsSync = function(){
-        var filepath = process.env.PWD + '/backups/hosts-' + new Date().getTime() + '.bak'
-        fs_sync.copy('/private/etc/hosts', filepath)
+    var backupHostsSync = function() {
+        var filepath = conf.backupDir + '/hosts-' + new Date().toString().split(' ').slice(0,5).join('-').replace(/\:/g, '.') + '.bak'
+        fs_sync.copy(conf.hostsLocation, filepath)
         return filepath
     }
 
+    /**
+     * Write new data to the hosts file. This function is synchronous
+     * 
+     * @param  {array} data 
+     * @return {[type]}      [description]
+     */
+    var writeHostsFileSync = function(data) {
+
+        if(conf.backups === true){
+            console.log("Backing up hosts file...".yellow)
+            var backup = backupHostsSync()
+            console.log("Backup saved to ".yellow, backup.yellow.underline)
+        }
+
+        // var orig = fs_sync.read(process.env.PWD + '/hosts')
+        //   , head = orig.slice(0, orig.indexOf(conf.startBanner))
+        //   , body = orig.slice(orig.indexOf(conf.startBanner), orig.indexOf(conf.endBanner) + conf.endBanner.length)
+        //   , tail = orig.slice(orig.indexOf(conf.endBanner) + conf.endBanner.length)
+        
+        var rawData = ''
+
+        for(var i = 0; i < data.entries.length; i++) {
+            var txt = data.enabled === true ? '' : '#'
+            txt += conf.rerouteIP + ' ' + data[i] +'\n'
+            rawData += txt
+        }
+
+        fs_sync.write(conf.hostsLocation, data.raw.head + conf.startBanner + '\n' + rawData + conf.endBanner + data.raw.tail)
+    }//({enabled: true, entries: ['www.reddit.com', 'np.reddit.com', 'reddit.com'] })
 
     // ************** Priviliged methods ************** //
 
     /**
-     * Parse the system's hosts file for Stopp rules
+     * Parse the system's hosts file for Stopp rules. This function is synchronous
      * 
      * @param  {Function} callback  Callback function, params: (error, data)
+     * @return {Object}             Object of format {enabled: bool, entries: [{ip: ip, address: address}]}
      */
-    this.parseHosts = function(callback) {
-        fs.readFile('/private/etc/hosts', 'utf8', function(err, data) {
+    this.parseHostsSync = function() {
+
+
+
+        var rawText           = fs_sync.read(conf.hostsLocation)
+          // , lines             = rawText.split('\n')
+          , startBannerCount  =  rawText.split(conf.startBanner).length-1//(rawText.match(new RegExp(conf.startBanner), 'gm') || []).length 
+          , endBannerCount    =  rawText.split(conf.endBanner).length-1//(rawText.match(new RegExp(conf.endBanner), 'gm') || []).length
+          , bannersPresent    =  startBannerCount === 1 && endBannerCount === 1
+          , bannersAbsent     =  startBannerCount + endBannerCount === 0
+          , bannerMismatch    = !(bannersPresent || bannersAbsent) 
+
+        if(bannerMismatch) {
+            console.log("\nStopp: Error: Hosts file is corrupt: Banner mismatch".red)
+            console.log("Fix the issue or restore a backup from ".yellow, conf.backupDir.yellow.underline)
+            console.log("Debug information:")
+            console.log("\tstartBannerCount: "+ startBannerCount)
+            console.log("\tendBannerCount: "+ endBannerCount)
+            process.exit(1)
+        }
+
+        // console.log("startBannerCount", startBannerCount, "\nendBannerCount", endBannerCount, "\nbannersPresent: ", bannersPresent, "\nbannersAbsent", bannersAbsent, "\ncorrupt", corrupt)
+
+        var startBannerIndex  = rawText.indexOf(conf.startBanner)
+          , endBannerIndex    = rawText.indexOf(conf.endBanner)
+        
+        if(startBannerIndex > endBannerIndex) {
+            console.log("\nStopp: Error: Hosts file is corrupt: Banners in wrong order".red)
+            console.log("Fix the issue or restore a backup from ".yellow, conf.backupDir.yellow.underline)
+            process.exit(1)
+        }
+
+        // Data model
+        var data = { enabled: false
+                   , raw: { full: rawText
+                          , head: bannersPresent ? rawText.slice(0, startBannerIndex) : rawText
+                          , body: bannersPresent ? rawText.slice(startBannerIndex + conf.startBanner.length, endBannerIndex) : '' // Not including banners
+                          , tail: bannersPresent ? rawText.slice(endBannerIndex + conf.endBanner.length) : ''
+                          }
+                   , entries: []
+                   }
+        
+
+        if(!bannersPresent || !data.raw.body) {
+            return data
+        }
+
+        var rawEntries = data.raw.body.split('\n')
+
+        for(var i = 0; i < rawEntries.length; i++){
+
+            var entry = rawEntries[i].replace(/\s+/g, " ").trim() // Get rid of extra whitespace
             
-            if(err) {
-                callback(err)
-                return
+            if(entry === '') continue
+            
+            if(entry[0] === '#'){
+                entry = entry.slice(1) 
+            } else {
+                data.enabled = true  // If even one entry is enabled, we'll consider the entire filter to be "ON" (There shouldn't usually be a mixture of enabled/disabled entries)
             }
 
-            var lines = data.split('\n')
-              , foundStartBanner = false
-              , entries = []
-            
-            for(var i = 0; i < lines.length; i++){
-                if(foundStartBanner === true && lines[i] !== ''){
-                    if(lines[i] === conf.endBanner) break
-                
-                    var l = lines[i].trim()
-                      , entry = {}
-                    
-                    if(l[0] === '#'){
-                        entry.enabled = false
-                        l = l.slice(1)
-                    } else {
-                        entry.enabled = true
-                    }
+            entry = entry.split(" ") // Split into array on spaces
 
-                    l = l.replace(/\s+/g, " ").split(" ")
+            if(entry.length !== 2) continue // Valid entries should be of length 2
+        
+            data.entries.push(entry[1]) // Just push the actual address, we don't really care about the IP
 
-                    if(l.length !== 2) continue
-                
-                    entry.ip = l[0]
-                    entry.address = l[1]
+        }
 
-                    entries.push(entry)
-
-                } else if(lines[i] === conf.startBanner) {
-                    foundStartBanner = true
-                }
-            }
-            
-            callback(false, entries)
-
-        })
+        return data
     }
+
+    
 
 
     if(argv.length > 0 && typeof this[argv[0]] === 'function' && this[argv[0]].callable === true){
         return this[argv[0]](argv.splice(1))      
     } else {
-        if(argv.length > 0) console.log("Invalid option: " + argv[0]) 
+        if(argv.length > 0) console.log("Stopp: Invalid option: ".red, argv[0].yellow) 
         this.help()
     }
 }
+
+// ************** Public methods ************** //
 
 /**
  * Help Method
@@ -132,13 +205,13 @@ function Stopp(argv){
  * Displays usage information
  */
 Stopp.prototype.help = function() {
-    conf.help.forEach(function(ele) {
-        console.log(ele)
+    conf.help.forEach(function(text) {
+        console.log(text)
     })
     
     return 0
 }
-Stopp.prototype.help.callable = true
+Stopp.prototype.help.callable = true // Callable from command-line (ex: `stopp help` causes this method to fire)
 
 /**
  * On Method
@@ -147,22 +220,80 @@ Stopp.prototype.help.callable = true
  *
  */
 Stopp.prototype.on = function() {
-    console.log("You called the 'on' method.")
+    var rules = this.parseHostsSync()
 
+    if(rules.entries.length === 0){
+        console.log("\nStopp: Error: Empty ruleset.\n".red)
+        return
+    }
+    if(rules.enabled === true){
+        console.log("\nStopp: Error: Stopp is already ".red+"ON\n".green.underline)
+        return
+    }
 
 }
 Stopp.prototype.on.callable = true
 
+/**
+ * Off Method
+ * 
+ * Turn filter ON
+ *
+ */
+Stopp.prototype.off = function() {
+    var rules = this.parseHostsSync()
+
+    if(rules.entries.length === 0){
+        console.log("\ntopp: Error: Empty ruleset.\n".red)
+        return
+    }
+    if(rules.enabled === false){
+        console.log("\nStopp: Error: Stopp is already ".red+"OFF\n".red.underline)
+        return
+    }
+}
+Stopp.prototype.off.callable = true
+
 Stopp.prototype.status = function() {
-    this.parseHosts(function(err, data){
-        console.dir(data)
-        if(data.length > 0 && data[0].enabled === true){
-            console.log("Stopp is ON.")
-        }else{
-            console.log("Stopp is OFF.")
-        }
-    })
+    
+    var rules = this.parseHostsSync()
+
+    if(rules.enabled === true){
+        console.log("\nStopp is ".yellow+"ON\n".green.underline)
+    } else {
+        console.log("\nStopp is ".yellow+"OFF\n".red.underline)
+    }
 }
 Stopp.prototype.status.callable = true
+
+Stopp.prototype.list = function() {
+    
+    var rules = this.parseHostsSync()
+
+    if(rules.enabled === true){
+        console.log("\nStopp status: ".yellow+"ON\n".green.underline)
+    } else {
+        console.log("\nStopp status: ".yellow+"OFF\n".red.underline)
+    }
+
+    if(rules.entries.length > 0){
+        console.log("Rules:".yellow)
+        for(var i = 0; i < rules.entries.length; i++){
+            console.log('\t' + rules.entries[i].cyan)
+        }
+    } else {
+        console.log("\tEmpty ruleset.".red)
+    }
+    console.log("")
+}
+Stopp.prototype.list.callable = true
+
+Stopp.prototype.add = function(domains) {
+    
+    var rules = this.parseHostsSync()
+
+    console.dir(rules)
+}
+Stopp.prototype.add.callable = true
 
 return new Stopp(yargs.argv._)
